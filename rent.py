df3c2b7c83cae0d76411f4e703b3ef58a18177cb
Zack Hsi <zackhsi@gmail.com>
2016-05-31 22:19:37 -0700

    Revert "Prorate last month"
    This reverts commit 7b21d5f54355a1f83237025df5dc64d1754deabd.

--- rent.py
+++ rent.py
@@ -9,15 +9,13 @@ I need to cash out $6546.95
 
 import venmo
 
-last_month_ratio = 19 / 30.0
-
 CONVENIENCE_FEE = 5
 CHARGES = {
-    '@nicholas-lippis': last_month_ratio * 1920.04 + CONVENIENCE_FEE,
-    '@dylan-harris-3': last_month_ratio * 1675.04 + CONVENIENCE_FEE,
-    '@robert-garbanati': last_month_ratio * 976.28 + CONVENIENCE_FEE,
-    '@zmorris': last_month_ratio * 1000,
-    '@traplord': last_month_ratio * 976.29 + CONVENIENCE_FEE,
+    '@nicholas-lippis': 1920.04 + CONVENIENCE_FEE,
+    '@dylan-harris-3': 1675.04 + CONVENIENCE_FEE,
+    '@robert-garbanati': 976.28 + CONVENIENCE_FEE,
+    '@zmorris': 1000,
+    '@traplord': 976.29 + CONVENIENCE_FEE,
 }
 
 
