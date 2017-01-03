b9e3caaedaa90dc6229040d5dc622f4824ee56c5
Zack Hsi <zackhsi@gmail.com>
2017-01-03 10:34:12 +0100

    Update rents for Ashley

--- rent.py
+++ rent.py
@@ -2,28 +2,36 @@
 """
 Charge my roommates rent.
 
-I pay 1266.67
+I pay 1247.61
 Total is 10750
-I need to cash out 9483.33
+I need to cash out 9502.39
 """
 
 import venmo
 
-ME = 1266.67
+ME = 1247.61
 CHARGES = {
-    '@nicholas-lippis': 1600,
-    '@dylan-harris-3': 1600,
-    '@robert-garbanati': 1700,
-    '@zmorris': 1500,
-    '@traplord': 1325,
-    '@ben-hunter-3': 1125,
-    '@Claire-Redemer': 633.33,
+    '@nicholas-lippis': 1571.43,
+    '@dylan-harris-3': 1771.43,
+    '@robert-garbanati': 1671.43,
+    '@zmorris': 1471.43,
+    '@traplord': 1296.43,
+    '@ben-hunter-3': 1096.43,
+    '@Claire-Redemer': 623.81,
 }
 TOTAL = 10750
+TOLERANCE = 0.01
 
 
 def main():
-    assert ME + sum(CHARGES.values()) == TOTAL
+    total_charged = ME + sum(CHARGES.values())
+    if abs(TOTAL - total_charged) > TOLERANCE:
+        print('Rent {} does not equal total {}'
+              .format(
+                  ME + sum(CHARGES.values()),
+                  TOTAL,
+              ))
+        return
     for user, amount in CHARGES.iteritems():
         venmo.payment.charge(user, amount, 'rent')
 
